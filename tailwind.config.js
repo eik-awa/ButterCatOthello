65ff17c4bd075620395bc3cfae5e1b7b7aec857c
eiki-ogawa <eiki-ogawa@x-power.co.jp>
2025-10-16 21:26:07 +0900

    feat: github pages tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,10 @@
 module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}", // if using pages directory
+    "./components/**/*.{js,ts,jsx,tsx,mdx}", // all components directory
+    "./app/**/*.{js,ts,jsx,tsx,mdx}", // if using app directory
+    "./styles/**/*.{css,scss}",
+  ],
   theme: {
     extend: {
       colors: {},
